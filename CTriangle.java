ae81ca26a67f9875968385e0257cc50f74512af1
0624010 <55846446+0624010@users.noreply.github.com>
2019-11-21 16:27:02 +0800

    Update CTriangle.java

--- CTriangle.java
+++ CTriangle.java
@@ -1,16 +1,25 @@
-Class CTriangle extends CShape
+abstract class Cshape
+{
+    protected String color;
+    public void setColor(String str)
+    {
+        color=str;
+    }
+public abstract void show();
+}
+
+class CTriangle extends CShape
     {
     protected double a,b,c
-
- public CTriangle (double x , double y , double z)
+    public CTriangle (double x , double y , double z)
     (
       a=x;
       b=y;
       c=z;
     )
-public void show()
+    public void show()
     {
-System.out.print("面積="a*b*0.5);
+    System.out.print("面積="a*b*0.5);
     }
     }
 
