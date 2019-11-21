e3bdae9676719f28b8b0903f5bb0d9c6bc2e0d5c
0624010 <55846446+0624010@users.noreply.github.com>
2019-11-21 16:10:05 +0800

    Create CTriangle.java

---
+++ CTriangle.java
@@ -0,0 +1,16 @@
+Class CTriangle extends CShape
+    {
+    protected double a,b,c
+
+ public CTriangle (double x , double y , double z)
+    (
+      a=x;
+      b=y;
+      c=z;
+    )
+public void show()
+    {
+System.out.print("面積="a*b*0.5);
+    }
+    }
+
